83cc86b074ae5962c1e37af6efbecaeef60bb1cc
kshaw031 <kevin.shaw143@gmail.com>
2023-05-27 23:32:23 -0700

    Code used for Remix Action exploration and prototyping database record insertion

--- app/routes/entries.tsx
+++ app/routes/entries.tsx
@@ -1,19 +1,25 @@
-import type { LoaderArgs } from "@remix-run/node"
+import { ActionArgs, ActionFunction, LoaderArgs, redirect } from "@remix-run/node"
 import { json } from "@remix-run/node"
-import { Outlet } from "@remix-run/react"
+import { Form, Outlet, useLoaderData } from "@remix-run/react"
 import Header from "~/components/elements/Header"
 import Main from "~/components/elements/Main"
 import Nav from "~/components/elements/Nav"
 
-import { getEntries } from "~/models/entry.server"
+import { getEntries, putEntries } from "~/models/entry.server"
 
 export async function loader({ request }: LoaderArgs) {
   const entries = await getEntries()
   return json({ entries })
 }
 
+export async function action({ request }: ActionArgs) {
+  const data = Object.fromEntries(await request.formData());
+  putEntries(data);
+  return redirect(`/entries/${data.inputBox}`);
+}
+
 export default function EntriesPage() {
-  // const data = useLoaderData<typeof loader>();
+  const data = useLoaderData<typeof loader>();
 
   return (
     <div className="relative">
@@ -22,6 +28,26 @@ export default function EntriesPage() {
       <Main>
         <Outlet />
       </Main>
+      <Form id="newForm" className=" bg-gray-400 relative mt-20 p-3 md:mx-auto md:mt-36 md:flex md:flex-col md:justify-center" method="post">
+        <label className="p-3">
+          insert data
+          <input className="m-3" type="text" name="inputBox" />
+        </label>
+        <label className="p-3">
+          insert more data
+          <input className="m-3" type="text" name="inputBox2" />
+        </label>
+        <button className="bg-white" type="submit" name="submitButton">
+          Submit
+        </button>
+      </Form>
+
+      {data.entries.map((e) => {
+        return (<p className="relative mt-20 bg-white p-3 md:mx-auto md:mt-36 md:flex md:flex-row md:justify-center">
+          {e.headword}
+        </p>);
+      })}
+
     </div>
   )
 }
