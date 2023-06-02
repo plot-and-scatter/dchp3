82674fb7d583df5379eb72a8ed542e112815cb03
kshaw031 <kevin.shaw143@gmail.com>
2023-06-02 15:04:34 -0700

    Code cleanup

--- app/routes/entries.tsx
+++ app/routes/entries.tsx
@@ -1,20 +1,9 @@
-import { LoaderArgs, } from "@remix-run/node"
-import { json } from "@remix-run/node"
-import { Outlet, useLoaderData } from "@remix-run/react"
+import { Outlet } from "@remix-run/react"
 import Header from "~/components/elements/Header"
 import Main from "~/components/elements/Main"
 import Nav from "~/components/elements/Nav"
 
-import { getEntries } from "~/models/entry.server"
-
-export async function loader({ request }: LoaderArgs) {
-  const entries = await getEntries()
-  return json({ entries })
-}
-
 export default function EntriesPage() {
-  const data = useLoaderData<typeof loader>()
-
   return (
     <div className="relative">
       <Header />
--- app/routes/insertEntry.tsx
+++ app/routes/insertEntry.tsx
@@ -1,10 +1,9 @@
-import { ActionArgs, redirect } from "@remix-run/node";
+import { type ActionArgs, redirect } from "@remix-run/node"
 import { Form } from "@remix-run/react"
 import Header from "~/components/elements/Header"
 import Main from "~/components/elements/Main"
 import Nav from "~/components/elements/Nav"
-import { putEntries } from "~/models/entry.server";
-
+import { putEntries } from "~/models/entry.server"
 
 export async function action({ request }: ActionArgs) {
   const data = Object.fromEntries(await request.formData())
@@ -20,7 +19,11 @@ export default function Index() {
       <Main>
         <p>Insert Entry</p>
       </Main>
-      <Form id="newForm" className=" bg-gray-400 relative mt-10 p-3 md:mx-auto md:mt-18 md:flex md:flex-col md:justify-center" method="post">
+      <Form
+        id="newForm"
+        className=" md:mt-18 relative mt-10 bg-gray-400 p-3 md:mx-auto md:flex md:flex-col md:justify-center"
+        method="post"
+      >
         <label className="p-3">
           ID
           <input className="m-3" type="text" name="id" />
