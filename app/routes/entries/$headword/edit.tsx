df5c840de15d4b439142b45f3b6bb442e499fb0f
kshaw031 <kevin.shaw143@gmail.com>
2023-06-09 16:17:25 -0700

    added sample update entry form

--- app/routes/entries/$headword/edit.tsx
+++ app/routes/entries/$headword/edit.tsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "@remix-run/react"
+import { Form, useLoaderData } from "@remix-run/react"
 import { type LoadedDataType } from "."
 import { type LoaderArgs } from "@remix-run/server-runtime"
 import invariant from "tiny-invariant"
@@ -19,7 +19,41 @@ export default function EditEntryPage() {
 
   return (
     <div>
-      <p> Work In Progress. Entry: {data.headword}</p>
+      <h1 className="text-2xl font-bold"> Update Entry</h1>
+      <p>
+        Edit fields below to update the entry: <b>{data.headword}</b>
+      </p>
+      <Form className="my-5" method="post">
+        <div className="flex flex-col">
+          <label className="">
+            Headword
+            <input
+              className="mx-2 my-4 border p-1"
+              value={data.headword}
+            ></input>
+          </label>
+          <label className="">
+            Alternatives
+            <input
+              className="mx-2 my-4 border p-1"
+              value={"sample text"}
+            ></input>
+          </label>
+          <label className="">
+            Next Field
+            <input
+              className="mx-2 my-4 border p-1"
+              value={"sample text"}
+            ></input>
+          </label>
+          <button
+            className="mt-5 border border-slate-600 bg-slate-500 p-1 text-white hover:bg-slate-400"
+            type="submit"
+          >
+            Update Entry
+          </button>
+        </div>
+      </Form>
     </div>
   )
 }
