bf15b710e785b4848bd3d6327a284c0ba25ba944
kshaw031 <kevin.shaw143@gmail.com>
2023-07-04 19:52:18 -0700

    Add links, temp commit

--- app/routes/search.tsx
+++ app/routes/search.tsx
@@ -1,4 +1,4 @@
-import { Form, Outlet, useParams } from "@remix-run/react"
+import { Form, Link, Outlet, useParams } from "@remix-run/react"
 import { type ActionArgs, redirect } from "@remix-run/server-runtime"
 import { useState } from "react"
 import Header from "~/components/elements/Header"
@@ -29,37 +29,46 @@ export default function SearchPage() {
         <div className="flex max-w-4xl flex-col">
           <h1 className="text-2xl font-bold">Search entries</h1>
           <p>Enter search text to find headwords containing that text.</p>
-          <Form method="post">
-            <div className="flex w-4/5 flex-col gap-3 p-1">
-              <input
-                type="text"
-                placeholder="Search text"
-                className="border border-slate-700 p-2"
-                name="searchText"
-                value={text}
-                onChange={(e) => {
-                  setText(e.target.value)
-                }}
-              />
+          <div className="flex flex-row justify-around">
+            <Form method="post">
+              <div className="flex flex-col gap-3 p-1">
+                <input
+                  type="text"
+                  placeholder="Search text"
+                  className="border border-slate-700 p-2"
+                  name="searchText"
+                  value={text}
+                  onChange={(e) => {
+                    setText(e.target.value)
+                  }}
+                />
 
-              <div className="ml-2 flex justify-around">
-                <label>
-                  Case-sensitive:
-                  <input
-                    className="ml-3"
-                    name="caseSensitive"
-                    type="checkbox"
-                    value="true"
-                  />
-                </label>
+                <div className="ml-2 flex justify-around">
+                  <label>
+                    Case-sensitive:
+                    <input
+                      className="ml-3"
+                      name="caseSensitive"
+                      type="checkbox"
+                      value="true"
+                    />
+                  </label>
+                </div>
+
+                <button className="ml-3 border border-slate-600 bg-slate-500 p-2 text-white hover:bg-slate-400">
+                  <i className="fas fa-search mr-2"></i>
+                  Search
+                </button>
               </div>
-
-              <button className="ml-3 border border-slate-600 bg-slate-500 p-2 text-white hover:bg-slate-400">
-                <i className="fas fa-search mr-2"></i>
-                Search
-              </button>
+            </Form>
+            <div className="flex flex-col pl-5">
+              <Link to="hi"> hi </Link>
+              <Link to="hi"> hi </Link>
+              <Link to="hi"> hi </Link>
+              <Link to="hi"> hi </Link>
+              <Link to="hi"> hi </Link>
             </div>
-          </Form>
+          </div>
           <Outlet />
         </div>
       </Main>
