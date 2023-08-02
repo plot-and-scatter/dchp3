91035c7675b38c70e8bf5b609f74a7a6603097a7
Frank Hangler <frank@plotandscatter.com>
2023-08-02 14:15:01 -0700

    Fix destructuring of args

--- app/routes/bank/index.tsx
+++ app/routes/bank/index.tsx
@@ -1,7 +1,8 @@
+import type { LoaderArgs } from "@remix-run/server-runtime"
 import { PageHeader } from "~/components/elements/PageHeader"
 import { redirectIfUserNotLoggedIn } from "~/services/auth/session.server"
 
-export const loader = async (request: Request) => {
+export const loader = async ({ request }: LoaderArgs) => {
   await redirectIfUserNotLoggedIn(request)
 
   return {}
