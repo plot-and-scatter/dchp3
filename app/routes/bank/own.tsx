e2d8a9b92b0d1be748c543b41d24c14e5038e674
Frank Hangler <frank@plotandscatter.com>
2023-08-22 13:25:23 -0700

    Fix own listing

--- app/routes/bank/own.tsx
+++ app/routes/bank/own.tsx
@@ -1,3 +1,4 @@
+import { getOwnCitations } from "~/models/bank.server"
 import { json, type LoaderArgs } from "@remix-run/server-runtime"
 import { useLoaderData } from "@remix-run/react"
 import { PageHeader } from "~/components/elements/PageHeader"
@@ -5,8 +6,6 @@ import {
   getEmailFromSession,
   redirectIfUserLacksPermission,
 } from "~/services/auth/session.server"
-import { prisma } from "~/db.server"
-import type { BankGetOwnCitation } from "~/models/bank.types"
 import BankCitationResult from "~/components/bank/BankCitationResult"
 
 export const loader = async ({ request }: LoaderArgs) => {
@@ -14,9 +13,7 @@ export const loader = async ({ request }: LoaderArgs) => {
 
   const email = await getEmailFromSession(request)
   if (!email) throw json({ message: `No email on user` }, { status: 500 })
-  const citations = await prisma.$queryRaw<
-    BankGetOwnCitation[]
-  >`SELECT c.user_id, c.last_modified_user_id, c.text, c.id, c.short_meaning, c.spelling_variant, c.created, c.last_modified, h.headword, c.source_id, s.year_published, s.type_id, s.year_composed, p.name as place_name, u.email FROM citation AS c, headword AS h, source AS s, place AS p, user AS u WHERE c.headword_id=h.id AND c.source_id=s.id AND s.place_id=p.id AND c.user_id=u.id ORDER BY c.created DESC`
+  const citations = await getOwnCitations(email)
 
   return citations
 }
@@ -32,4 +29,20 @@ export default function OwnCitations() {
       ))}
     </>
   )
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  console.error(error)
+
+  return <div>An unexpected error occurred: {error.message}</div>
+}
+
+export function CatchBoundary() {
+  const caught = useCatch()
+
+  if (caught.status === 404) {
+    return <div>Entry not found</div>
+  }
+
+  throw new Error(`Unexpected caught response with status: ${caught.status}`)
 }
