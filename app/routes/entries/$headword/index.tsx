64c8cae4c3e96e7b67f3243c14e90c030e85c302
kshaw031 <kevin.shaw143@gmail.com>
2023-09-02 20:38:17 -0700

    Cleanup unused files

--- app/routes/entries/$headword/index.tsx
+++ app/routes/entries/$headword/index.tsx
@@ -1,30 +1,11 @@
 import type { Prisma } from "@prisma/client"
-import { redirect, type ActionArgs, type LoaderArgs } from "@remix-run/node"
+import { type LoaderArgs } from "@remix-run/node"
 import { useCatch, useLoaderData } from "@remix-run/react"
 import invariant from "tiny-invariant"
 
 import { getEntryByHeadword } from "~/models/entry.server"
-import {
-  addDefinitionFistNote,
-  addSeeAlso,
-  deleteSeeAlso,
-  updateCanadianism,
-  updateOrDeleteDefinitionFistNote,
-  updateEditingStatus,
-  updateEditingTools,
-  updateMeaningDefinition,
-  updateMeaningHeader,
-  updateRecordByAttributeAndType,
-  addMeaningToEntry,
-} from "~/models/update.server"
 
 import Entry from "~/components/Entry"
-import {
-  getAttributeEnumFromFormInput,
-  getStringFromFormInput,
-} from "~/utils/generalUtils"
-import { attributeEnum } from "~/components/editing/attributeEnum"
-
 export async function loader({ params }: LoaderArgs) {
   invariant(params.headword, "headword not found")
 
@@ -33,55 +14,6 @@ export async function loader({ params }: LoaderArgs) {
     throw new Response("Not Found", { status: 404 })
   }
   return entry
-}
-
-export async function action({ request }: ActionArgs) {
-  const data = Object.fromEntries(await request.formData())
-  const type = getAttributeEnumFromFormInput(data.attributeType)
-
-  switch (type) {
-    case attributeEnum.ADD_MEANING:
-      await addMeaningToEntry(data)
-      break
-    case attributeEnum.MEANING_HEADER:
-      await updateMeaningHeader(data)
-      break
-    case attributeEnum.SEE_ALSO:
-      await addSeeAlso(data)
-      break
-    case attributeEnum.DELETE_SEE_ALSO:
-      await deleteSeeAlso(data)
-      break
-    case attributeEnum.CANADIANISM:
-      await updateCanadianism(data)
-      break
-    case attributeEnum.DEFINITION:
-      await updateMeaningDefinition(data)
-      break
-    case attributeEnum.DEFINITION_FIST_NOTE:
-      await updateOrDeleteDefinitionFistNote(data)
-      break
-    case attributeEnum.ADD_DEFINITION_FIST_NOTE:
-      await addDefinitionFistNote(data)
-      break
-    case attributeEnum.EDITING_TOOLS:
-      await updateEditingTools(data)
-      break
-    case attributeEnum.EDITING_STATUS:
-      await updateEditingStatus(data)
-      break
-    default:
-      await updateRecordByAttributeAndType(type, data)
-      break
-  }
-
-  // old headword invalid-- redirect to updated headword
-  if (type === attributeEnum.HEADWORD) {
-    const headword = getStringFromFormInput(data.newValue)
-    return redirect(`/entries/${headword}`)
-  }
-
-  return null
 }
 
 export type LoadedDataType = Prisma.PromiseReturnType<typeof loader>
