87b82fadf86b25c8be9063f3485b1a972f411c41
kshaw031 <kevin.shaw143@gmail.com>
2023-10-10 13:19:59 -0700

    Make Entry List (in profile) display links and dates

--- app/components/profile/EntryList.tsx
+++ app/components/profile/EntryList.tsx
@@ -1,17 +1,36 @@
 import React from "react"
 import { type LogEntries } from "~/models/user.server"
+import { Link } from "../elements/LinksAndButtons/Link"
 
 interface EntryListProps {
   logEntries: LogEntries
 }
 
 const EntryList = ({ logEntries }: EntryListProps) => {
-  console.log(logEntries)
   return (
-    <div>
-      {logEntries.map((e) => {
-        return <p key={e.id}>{e.Entry?.headword}</p>
-      })}
+    <div className="m-4">
+      <h2 className="text-3xl">User's Entries</h2>
+      <div className="grid grid-cols-9 [&>*]:text-lg">
+        {logEntries.map((e) => {
+          return (
+            <React.Fragment key={e.id}>
+              <p className="col-span-1">Entry:</p>
+              <Link
+                className="col-span-3"
+                bold
+                to={`/entries/${e.Entry?.headword}`}
+              >
+                {e.Entry?.headword ?? `Deleted entry: ${e.entry_id}`}
+              </Link>
+              <p className="col-span-1"></p>
+              <p className="col-span-1">Edited:</p>
+              <p className="col-span-3">
+                {new Date(e.created).toLocaleDateString()}
+              </p>
+            </React.Fragment>
+          )
+        })}
+      </div>
     </div>
   )
 }
