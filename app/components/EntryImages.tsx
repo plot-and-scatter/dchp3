23217a1446dc8c1407c172d600f483ccb939b129
Frank Hangler <frank@plotandscatter.com>
2023-10-16 09:15:29 -0700

    Sort images correctly. Fix #203

--- app/components/EntryImages.tsx
+++ app/components/EntryImages.tsx
@@ -2,11 +2,15 @@ import { EntryImage } from "./EntryImage"
 import { type LoadedEntryDataType } from "~/routes/entries/$headword"
 
 const EntryImages = ({ data }: { data: LoadedEntryDataType }): JSX.Element => {
+  const images = data.images
+    .filter((i) => i.order !== null)
+    .sort((a, b) => (a.order || 0) - (b.order || 0))
+
   return (
     <div>
       <h1 className="mb-5 text-2xl font-bold">Images</h1>
       <div className="flex flex-col items-center justify-around">
-        {data.images.map((image) => (
+        {images.map((image) => (
           <EntryImage key={image.id} image={image} />
         ))}
       </div>
