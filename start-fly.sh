8c8fbb57c3ef3a7972fd0c0cc389188016b228d0
Frank Hangler <frank@plotandscatter.com>
2024-01-19 08:32:24 -0800

    Startup tweaks

--- start-fly.sh
+++ start-fly.sh
@@ -5,6 +5,9 @@
 # run, which is why this file exists in the first place.
 # Learn more: https://community.fly.io/t/sqlite-not-getting-setup-properly/4386
 
+# FH added 2024-01-19: This file is only used when running on Fly. To run the
+# app on a server that's NOT Fly, use the `start-shell.sh` script.
+
 set -ex
 #npx prisma migrate deploy
 npm run start
